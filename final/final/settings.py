1af5a58f3e53ee836c2bf09671f60d7313e89455
Ezequiel <stambuliezequiel@gmail.com>
2025-12-16 22:21:23 -0300

    se modifico settings para dejar el debug en false

--- final/final/settings.py
+++ final/final/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k=(sfj0_b1hfc33iu=82bzz^!!#tg^2l2f13tisu=qhpyf88wl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
